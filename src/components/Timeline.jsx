6c18b018b12a48ff99604466520ca61e6a823a75
Miki <ml227cr@student.lnu.se>
2025-08-01 21:27:28 +0200

    Add timeline with animation applied

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,54 +5,61 @@ import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
-const Timeline = () => {
-  // `scope` is used as a reference for which elements to target
-  const scope = useRef()
+const timelineItems = [
+  { year: "2020", title: "Telia", description: "placeholder" },
+  { year: "2023", title: "Flyttar till Spanien", description: "placeholder" },
+  { year: "2024", title: "Webbprogrammerare @ Linnéuniversitetet", description: "placeholder" },
+  { year: "2025", title: "SI-ledare & TA @ Linnéuniversitet", description: "placeholder" },
+];
 
-  // React-specific method, runs when component mounts <- what does that mean
+const Timeline = () => {
+  const tlContainer = useRef();
+
   useGSAP(() => {
-    // Initial setup of items
-    gsap.set(".timeline-item", {
-      y: 200, // Start value is 200px down from top
-      opacity: 0 // Start value is invisible
+    // Render timeline items to the DOM and animate them with GSAP & ScrollTrigger
+    const tlItem = gsap.utils.toArray(".timeline-item");
+    const tl = gsap.timeline(); // Master timeline
+
+    // Items alternate entering from left/right based on index
+    tlItem.forEach((item, i) => {
+      tl.from(item, {
+        xPercent: i % 2 === 0 ? -200 : 200,
+        opacity: 0,
+      });
     });
 
-    // Creates a timeline to animate all chosen items
-    const tl = gsap.timeline();
 
-      tl.to(".timeline-item", {
-        y: 0, // Animate back to original position
-        opacity: 1, // Fade in
-        duration: 2,
-        stagger: 0.7, // Delay each item's start by x after previous item
-        scrollTrigger: {
-          trigger: "#tl-container",
-        }
-      })
-    }, { scope }); // Affects only `scope`
-
- const timelineItems = [
-    { year: '2020', title: 'Telia', description: 'placeholder' },
-    { year: '2023', title: 'Flyttar till Spanien', description: 'placeholder' },
-    { year: '2024', title: 'Webbprogrammerare @ Linnéuniversitetet', description: 'placeholder' },
-
-    { year: '2025', title: 'SI-ledare & TA (Teacher Assistant) @ Linnéuniversitet', description: 'placeholder' },
-  ];
+    ScrollTrigger.create({
+      animation: tl,
+      trigger: tlContainer.current,
+      start: "top top",
+      end: "+=2500",
+      scrub: 1,
+      pin: true,
+      anticipatePin: 1,
+      markers: true, // development property
+    });
+  }, { scope: tlContainer});
 
   return (
-    <section id="timeline" className="p-10" ref={scope}>
-      <h2 className="text-3xl text-third-white py-6">My Timeline</h2>
-      <div id="tl-container" className="relative border-l-2 border-gray-300 pl-6">
-        {timelineItems.map((item, index) => (
-          <div key={index} className="timeline-item mb-10 text-wrap">
-            <div className="text-md text-secondary-white">{item.year}</div>
-            <div className="text-xl text-primary-white">{item.title}</div>
-            <div className="text-fourth-white">{item.description}</div>
-          </div>
-        ))}
-      </div>
-    </section>
+    // Container viewport height is to allow for some scrolling to reveal timeline
+    <div
+      ref={tlContainer}
+      id="tlContainer"
+      className="relative min-h-[1150px] overflow-hidden"
+    >
+      {timelineItems.map((item, i) => (
+        <div
+          key={i}
+          className="timeline-item p-8"
+        >
+          <h2 className="text-2xl font-bold">{item.title}</h2>
+          <p className="text-sm">{item.year}</p>
+          <p className="text-md">{item.description}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
-export default Timeline
+export default Timeline;

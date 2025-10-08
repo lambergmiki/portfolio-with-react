37f671780aa67bfc6aebdb99595ddea002139461
Miki Lamberg <ml227cr@student.lnu.se>
2025-10-08 22:00:24 +0200

    change layout of about section, add desired tech to tech stack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,67 +12,77 @@ const About = () => {
                 spendera tid med min familj.
             </p>
             <div className="tech-stack-container flex flex-col">
-                <p className="flex justify-center text-4xl tracking-wider font-extrabold">
-                    FRONTEND
+                <p className="text-5xl text-center tracking-wider font-extrabold mb-20">
+                    Tech Stack
+                </p>
+                <p className="flex justify-center text-2xl md:text-4xl font-extrabold mb-20">
+                    Comfort zone
                 </p>
 
-                <div className="flex justify-end">
-                    <div className="icon-container flex flex-col">
-                        <img
-                            src={"/icons/icons8-javascript-48.png"}
-                            alt="JavaScript logo"
-                            className=""
-                        />
+                <div className="icon-container flex flex-row justify-center mb-10 gap-2">
+                    <img
+                        src={"/icons/icons8-javascript-48.png"}
+                        alt="JavaScript logo"
+                        className=""
+                    />
 
-                        <img
-                            src={"/icons/icons8-css-48.png"}
-                            alt="CSS logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-html5-48.png"}
-                            alt="HTML5 logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-tailwind-css-48.png"}
-                            alt="TailwindCSS logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-react-48.png"}
-                            alt="React logo"
-                            className=""
-                        />
-                    </div>
+                    <img
+                        src={"/icons/icons8-css-48.png"}
+                        alt="CSS logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-html5-48.png"}
+                        alt="HTML5 logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-tailwind-css-48.png"}
+                        alt="TailwindCSS logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-express-js-48.png"}
+                        alt="Express.js logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-nodejs-48.png"}
+                        alt="Node.js logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-mongodb-48.png"}
+                        alt="MongoDB logo"
+                        className=""
+                    />
                 </div>
-
-                <p className="flex justify-center text-4xl tracking-wider font-extrabold">
-                    BACKEND
+            </div>
+            <div>
+                <p className="flex justify-center text-2xl md:text-4xl font-extrabold mt-10 mb-10">
+                    Vill utvecklas i...
                 </p>
-                <div className="flex justify-end">
-                    <div className="icon-container flex flex-col">
-                        <img
-                            src={"/icons/icons8-java-48.png"}
-                            alt="Java logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-express-js-48.png"}
-                            alt="Express.js logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-nodejs-48.png"}
-                            alt="Node.js logo"
-                            className=""
-                        />
-                        <img
-                            src={"/icons/icons8-mongodb-48.png"}
-                            alt="MongoDB logo"
-                            className=""
-                        />
-                    </div>
+                <div className="icon-container flex flex-row justify-center mb-10 gap-4">
+                    <img
+                        src={"/icons/icons8-react-48.png"}
+                        alt="React logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-typescript-48.png"}
+                        alt="Java logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-postgresql-48.png"}
+                        alt="Java logo"
+                        className=""
+                    />
+                    <img
+                        src={"/icons/icons8-java-48.png"}
+                        alt="Java logo"
+                        className=""
+                    />
                 </div>
             </div>
         </section>

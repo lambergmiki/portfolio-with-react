b865d8b9829ba346c8322e9535e4c6914a88333c
Miki Lamberg <ml227cr@student.lnu.se>
2025-10-09 17:40:54 +0200

    remove unused 3d model, clean up in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,10 @@
-import Food_truck from "../../public/Food_truck";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
-
 const Hero = () => {
     return (
         <section className="flex flex-row p-10 h-[500px]">
-            {/* Give headliner 1/3 of Hero container and 2/3 to 3d object */}
-
             <div className="">
                 <img
                     className="size-36 rounded-full lg:size-96 lg:mt-28"
-                    src="../../public/images/profilbild.jpg"
+                    src="/images/profilbild.jpg"
                     alt="Profile picture"
                 ></img>
                 <div className="flex flex-row">
@@ -20,17 +13,6 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
-
-            {/* 3D Canvas 1/3, currently disabled because I'm looking for alternative ideas for 3D models */}
-            {/* <div className="hidden lg:flex basis-1/3 p-10 pl-6 border">
-                <Canvas className="h-full w-full">
-                    <ambientLight />
-                    <OrbitControls />
-                    <Suspense fallback={null}>
-                        <Food_truck />
-                    </Suspense>
-                </Canvas>
-            </div> */}
         </section>
     );
 };

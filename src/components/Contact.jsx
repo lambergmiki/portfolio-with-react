cbee0da9fa9bbb15db70fe9d6bd1d45fa2dfd71c
Miki Lamberg <ml227cr@student.lnu.se>
2025-12-05 22:47:56 +0100

    Update work status and contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,23 +2,14 @@ const Contact = () => {
   return (
     <section id="contact" className="relative p-10 pl-6 mb-20 text-xl">
       <div className="icons-container flex flex-col">
-        <h2 className="text-5xl text-center text-secondary-white mb-10">Jag söker jobb!</h2>
+        <h2 className="text-5xl text-center text-secondary-white mb-10">Kontakta mig</h2>
         <p className="text-center text-balance mb-10 lg:text-2xl">
-          Inför stundande flytt tillbaka till Sverige söker jag mitt första jobb som utvecklare.
-          <br></br>
-          Tycker du att min profil verkar intressant? Tveka inte på att höra av dig. Du hittar mig
-          på...
+          Jag pratar gärna tech! <br></br>Du når mig enklast på mejl eller LinkedIn.
         </p>
 
         <div className="flex justify-center transition-transform duration-150 hover:rotate-12 ">
           <a href="mailto:miki@mikilamberg.me">
             <img src={"/icons/icons8-email-48.png"} alt="email icon" className="flex" />
-          </a>
-        </div>
-
-        <div className="flex justify-center transition-transform duration-150 hover:-rotate-12">
-          <a href="https://www.github.com/lambergmiki/" target="_blank">
-            <img src={"/icons/github50.png"} alt="GitHub logo" className="flex flex-col" />
           </a>
         </div>
 
